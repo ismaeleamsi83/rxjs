4545221bc45be32945d5b9c70c6bd8edb4af97e4
ismael <ismaelreyplata@gmail.com>
2024-02-05 21:08:59 +0100

    map y filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ObservablesComponent } from './observables/observables.component';
 import { IntervalTimerComponent } from './interval-timer/interval-timer.component';
 import { FromEventComponent } from './from-event/from-event.component';
+import { MapFilterComponent } from './map-filter/map-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ObservablesComponent,
     IntervalTimerComponent,
-    FromEventComponent
+    FromEventComponent,
+    MapFilterComponent
   ],
   imports: [
     BrowserModule,
